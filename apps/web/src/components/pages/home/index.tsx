c48738373dee165313eeb69739e176ff2fb1bdd4
bingis-dev <bingis@tcd.ie>
2025-12-09 18:59:04 +0000

    updated with npm install version

--- apps/web/src/components/pages/home/index.tsx
+++ apps/web/src/components/pages/home/index.tsx
@@ -4,7 +4,7 @@ import { DisasterMap } from "../../organisms/disasterMap";
 export const HomePage = () => (
   <HomeTemplate>
     <div className="space-y-6">
-      <h1 className="text-2xl font-bold">Disaster Response System</h1>
+      <h1 className="text-2xl font-bold">Disaster Response System </h1>
       <DisasterMap height="600px" />
     </div>
   </HomeTemplate>
